78009880cf855e3632b94e4f22414ad913a6e917
Harsh Kaushik n <55058037+Harshkaushik14@users.noreply.github.com>
2021-10-24 12:18:09 +0530

    Add files via upload
    incomplete

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    const year = "2021";
+    return (
+        <footer>
+            <p>Copyright {year}</p>
+        </footer>
+    )
+}
+
+export default Footer
